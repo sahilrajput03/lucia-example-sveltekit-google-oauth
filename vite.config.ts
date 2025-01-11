26d26f193d11cf7ee71cb12ddef92e62593e2788
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-11 16:20:17 +0530

    Update.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [sveltekit()],
 	preview: {
-		port: 10000 // to run on port 10000 on render environment
+		port: 10000 // to run on port 10000 on render environment because render's environment exposes 10000 port. Docs - https://render.com/docs/web-services#port-binding
 	}
 });
