a81f1be80acf7b243b5942abf8e422532bba0fae
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-11 16:23:48 +0530

    Update.

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,5 +1,8 @@
 import { Google } from "arctic";
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI } from "$env/static/private";
+import { ORIGIN, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI } from "$env/static/private";
+
+console.log('ORIGIN?', ORIGIN)
+// ORIGIN=http://localhost:3000
 
 export const google = new Google(
     GOOGLE_CLIENT_ID,
